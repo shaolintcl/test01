63e9084bf0e0d78f78b7aaa8f48c6cc23bcf8bc2
shaolintcl <shaolintcl@hotmail.com>
2022-11-06 16:49:48 +0800

    second commit

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ dic1 = {'aa':[1,2,3,4,5],'bb':['a','b','c','d','e']}
 
 df_1 = pd.DataFrame(dic1)
 print(df_1)
+
+df_1['cc'] = df_1.apply(lambda x : True if x['bb']=='a' else False,axis=1)
+print(df_1)
